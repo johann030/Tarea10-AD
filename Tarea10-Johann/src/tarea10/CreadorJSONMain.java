b7cdc4af45c4b523db0beaae7f15eab035f29aa2
Usuario <Usuario@DESKTOP-FNJ8BDH>
2024-11-05 22:33:19 +0100

    Cositas

--- Tarea10-Johann/src/tarea10/CreadorJSONMain.java
+++ Tarea10-Johann/src/tarea10/CreadorJSONMain.java
@@ -3,8 +3,9 @@ package tarea10;
 public class CreadorJSONMain {
 
 	public static void main(String[] args) {
-		System.out.println("a");
-
+		
+		CrearJSON CJ = new CrearJSON();
+		CJ.creador();
+		
 	}
-
 }
---
+++ Tarea10-Johann/src/tarea10/CrearJSON.java
@@ -0,0 +1,9 @@
+package tarea10;
+
+public class CrearJSON {
+	
+	public void creador() {
+		
+	}
+
+}
